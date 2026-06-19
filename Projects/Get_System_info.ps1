7d558f2c8bc9a2fc5e5fba737382ffd36834ec77
iradtaufique <iradukunda.ta@gmail.com>
2026-06-19 16:59:26 -0700

    getting RAM and Disk information using Get-PSDrive to get disk info and TotalVisibleMemorySize and FreePhysicalMemory to get RAM info

--- Projects/Get_System_info.ps1
+++ Projects/Get_System_info.ps1
@@ -17,8 +17,51 @@ Write-Host "HostName: $($hostname) `n"
 # Win32_OperatingSystem is the specific 'table' that hold os details.
 $os = Get-CimInstance -ClassName Win32_OperatingSystem
 
-Write-Host "Operating System: $($os.Caption)"
+Write-Host "Operating System: $($os.Caption)" # caption gets the name of the OS
 Write-Host "OS Version: $($os.Version)"
 
+# Win32_Processor is the WMI class or table for CPU Information.
+# it returns one object per physical cpu, so we grab the fisrt with [0]
+Write-Host "`n<--------> CPU Info <-------->" -ForegroundColor Cyan
+
+$cpu = Get-CimInstance -ClassName Win32_Processor
+
+Write-Host "CPU Name: $($cpu.Name)" # get cpu name
+Write-Host "Number of Core: $($cpu.NumberOfCores) Cores" # Number of core
+
+# Win32_OperatingSystem (which we store in $os variable) also holds memory info.
+# TotalVisibleMemorySize and FreePhysicalMemory are in KILOBYTES, so we
+# Divide by 1MB to Convert to GB.
+# [math]::Round() Rounds the number to 2 decimal places
+
+Write-Host "`n<-------> RAM Space <------->" -ForegroundColor Cyan
+
+$totalRAM = [math]::Round($os.TotalVisibleMemorySize / 1MB, 2)
+$freeRAM = [math]::Round($os.FreePhysicalMemory /1MB, 2)
+$usedRAM = [math]::Round($totalRAM - $freeRAM, 2)
+
+Write-Host "Total RAM: $($totalRAM) GB"
+Write-Host "Used RAM: $($usedRAM) GB"
+Write-Host "Free RAM: $($freeRAM) GB"
+
+# Get-PSDrive Retrieves all drives on the system
+# -PSProvider FileSystem filters to only real disk drives (not registry, ..etc)
+
+$drives = Get-PSDrive -PSProvider FileSystem # only get real disk drive
+Write-Host "`n<-------> Disk Space <------->" -ForegroundColor Cyan
+
+# loop through each drive
+foreach($drive in $drives){
+    # used and free drive are in bytes, so we will divide by 1GB to convert
+    $usedDrive = [math]::Round($drive.Used / 1GB, 2)
+    $freeDrive = [math]::Round($drive.Free /1GB, 2)
+    $totalDrive = [math]::Round(($drive.Used + $drive.Free) / 1GB, 2)
+
+    Write-Host "Drive Name $($drive.Name)"
+    Write-Host "Total Space: $($totalDrive) GB"
+    Write-Host "Used Space: $($usedDrive) GB"
+    Write-Host "Free Space: $($freeDrive) GB"
+
+}
 
 Write-Host "======== END SYSTEM INFORMATION REPORT ======== `n" -ForegroundColor Cyan
